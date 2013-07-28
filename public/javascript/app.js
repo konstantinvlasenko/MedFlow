f40a5f76d369c3518f347d9776c4fbcea41d46f3
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2013-07-28 18:49:31 -0400

    calendar

---
+++ public/javascript/app.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  var today = new Date(),
+    events = [
+       +new Date(today.getFullYear(), today.getMonth(), 8),
+       +new Date(today.getFullYear(), today.getMonth(), 12),
+       +new Date(today.getFullYear(), today.getMonth(), 24),
+       +new Date(today.getFullYear(), today.getMonth() + 1, 6),
+       +new Date(today.getFullYear(), today.getMonth() + 1, 7),
+       +new Date(today.getFullYear(), today.getMonth() + 1, 25),
+       +new Date(today.getFullYear(), today.getMonth() + 1, 27),
+       +new Date(today.getFullYear(), today.getMonth() - 1, 3),
+       +new Date(today.getFullYear(), today.getMonth() - 1, 5),
+       +new Date(today.getFullYear(), today.getMonth() - 2, 22),
+       +new Date(today.getFullYear(), today.getMonth() - 2, 27)
+    ];
+
+  $("#calendar").kendoCalendar({
+    value: today,
+    dates: events,
+    month: {
+      // template for dates in month view
+      content: '# if ($.inArray(+data.date, data.dates) != -1) { #' +
+                  '<div class="' +
+                     '# if (data.value < 10) { #' +
+                         "exhibition" +
+                     '# } else if ( data.value < 20 ) { #' +
+                         "party" +
+                     '# } else { #' +
+                         "cocktail" +
+                     '# } #' +
+                  '">#= data.value #</div>' +
+               '# } else { #' +
+               '#= data.value #' +
+               '# } #'
+    },
+    footer: false
+  });
+});
